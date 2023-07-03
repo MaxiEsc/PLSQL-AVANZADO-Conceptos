3c66858d93b17d79a5eaf07d09d68bff56fe20d8
Escobar Maximiliano <maxescobarubp@gmail.com>
2023-07-03 16:42:34 -0300

    Actualizacion JSON con JSON_TRANSFORM

--- JSON_MERGEPATH-JSON_TRANSFORM.sql
+++ JSON_MERGEPATH-JSON_TRANSFORM.sql
@@ -33,6 +33,23 @@ UPDATE productos1 SET datos = JSON_MERGEPATCH(datos, '{
 --RENAME
 --REPLACE
 --KEEP
-
+--Con JSON_TRANSFORM
 UPDATE productos1 SET datos = JSON_TRANSFORM(datos, SET '$.ciudad' = 'Barcelona')
 WHERE codigo = 2;
+--SELECT CON JSON TRANSFORM
+--En este caso le pido que me traiga los valores modificado de la tabla con la datos ya existentes. ademas 
+SELECT JSON_TRANSFORM(datos, SET '$.poblacion'= 20000000) "Seleccion"
+FROM productos1 WHERE codigo = 3;
+--Aqui vemos que tal cual se transforma la salida de los datos como se le indica, si no existe lo crea
+SELECT JSON_TRANSFORM(datos, SET '$.poblacion_BOSTA'= 999999999) "Seleccion Errornea"
+FROM productos1 WHERE codigo = 3;
+--EN este caso inserta una clave pero no se ingresa a la base pero si a la consulta
+SELECT JSON_TRANSFORM(datos, INSERT '$.ubicacion'= 'Europa') "Seleccion con Insert"
+FROM productos1 WHERE codigo = 2;
+--APPEND
+SELECT prod.datos.telefonos FROM productos1 prod;
+--Con append Agregamos un valor en el arreglo
+SELECT JSON_TRANSFORM(datos, APPEND '$.telefonos'= 158984516 ) "Seleccion con APPEND"
+FROM productos1 WHERE codigo = 3;
+
+
